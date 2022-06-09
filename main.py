2dd97fa7442da5677d396ae5adc536c3977282be
xvalor <davidkozlik11@gmail.com>
2022-06-09 08:11:46 +0200

    Operace 2

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-print("Sečti dvě čísla. Zadej je\n")
-prvnipromenna = int(input())
-druhapromenna = int(input())
+#print("Sečti dvě čísla. Zadej je\n")
+#prvnipromenna = int(input())
+#druhapromenna = int(input())
 
-vysledek = int(prvnipromenna + druhapromenna)
+#vysledek = int(prvnipromenna + druhapromenna)
 
-print(vysledek)
+#print(vysledek) První program. Commitnuto v Git
+
+
+#itNetworks - řešené úlohy k 1. a 2. lekci
+
+print("Ahoj, jak se jmenuješ?\n")
+jmeno = input()
+print("Jaký jsi?\n")
+vlasnost = input()
+print()
+
+
 
